c75fc046b038f47dbbb6394fe6f31fd0ff8adaea
Danik616 <ddanielvargas2011@gmail.com>
2023-04-18 17:37:00 -0500

    Se crean endpoints sumado a creación de la carpeta rutas

---
+++ bbdd.js
@@ -0,0 +1,319 @@
+export const USERS_BBDD= [
+    
+        {
+          "_id": "643f08278dd7b53f21e2cbd6",
+          "index": 0,
+          "guid": "3e255516-25f0-4e25-9965-0660b0eb92bd",
+          "isActive": true,
+          "balance": "$3,828.53",
+          "picture": "http://placehold.it/32x32",
+          "age": 37,
+          "eyeColor": "brown",
+          "name": "Lupe Walton",
+          "gender": "female",
+          "company": "COSMOSIS",
+          "email": "lupewalton@cosmosis.com",
+          "phone": "+1 (800) 439-3124",
+          "address": "218 Chase Court, Hemlock, Washington, 9797",
+          "about": "Eiusmod in laboris veniam dolore ex sit enim proident elit sunt elit. Deserunt est mollit voluptate laboris consectetur eiusmod Lorem culpa. Velit nostrud non proident esse officia laboris cillum est pariatur labore.\r\n",
+          "registered": "2020-12-05T11:14:12 +05:00",
+          "latitude": 44.289592,
+          "longitude": 78.108747,
+          "tags": [
+            "ex",
+            "anim",
+            "dolore",
+            "excepteur",
+            "et",
+            "ut",
+            "nulla"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Mays Murray"
+            },
+            {
+              "id": 1,
+              "name": "Garrett Mclaughlin"
+            },
+            {
+              "id": 2,
+              "name": "Elinor Wolf"
+            }
+          ],
+          "greeting": "Hello, Lupe Walton! You have 7 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "643f082798db73baeadb513e",
+          "index": 1,
+          "guid": "b856aab0-9e97-4b63-a130-55452e1f1055",
+          "isActive": true,
+          "balance": "$1,382.77",
+          "picture": "http://placehold.it/32x32",
+          "age": 39,
+          "eyeColor": "green",
+          "name": "Dora Reeves",
+          "gender": "female",
+          "company": "ZYPLE",
+          "email": "dorareeves@zyple.com",
+          "phone": "+1 (873) 590-2405",
+          "address": "869 Montrose Avenue, Tilleda, Oregon, 4303",
+          "about": "Aute mollit elit anim laborum ad sunt voluptate velit. Adipisicing do irure incididunt enim ex aliqua adipisicing consequat exercitation sit ullamco ex ipsum. Esse elit enim qui cillum laboris nostrud cupidatat exercitation mollit pariatur dolor. Lorem occaecat incididunt tempor consequat laborum ullamco commodo culpa irure. Consectetur cupidatat non ipsum voluptate. Laborum ad quis voluptate officia mollit nostrud reprehenderit eu commodo sint eiusmod ut voluptate.\r\n",
+          "registered": "2021-06-02T01:37:08 +05:00",
+          "latitude": 75.744397,
+          "longitude": 150.678972,
+          "tags": [
+            "amet",
+            "dolor",
+            "deserunt",
+            "mollit",
+            "nostrud",
+            "sint",
+            "adipisicing"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Carrie Fox"
+            },
+            {
+              "id": 1,
+              "name": "Conway Lloyd"
+            },
+            {
+              "id": 2,
+              "name": "Pansy Robles"
+            }
+          ],
+          "greeting": "Hello, Dora Reeves! You have 2 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "643f0827610b35498a6ffa1a",
+          "index": 2,
+          "guid": "eee32139-3af1-44f0-a24b-473367f64167",
+          "isActive": true,
+          "balance": "$1,920.22",
+          "picture": "http://placehold.it/32x32",
+          "age": 36,
+          "eyeColor": "green",
+          "name": "Susana Roberson",
+          "gender": "female",
+          "company": "ZILLADYNE",
+          "email": "susanaroberson@zilladyne.com",
+          "phone": "+1 (849) 461-3255",
+          "address": "544 Cook Street, Greer, Oklahoma, 1871",
+          "about": "Labore do tempor occaecat aliqua ipsum do labore mollit minim proident magna. Reprehenderit ut quis cillum eu non anim sint ea nulla veniam ad. Voluptate ad eu quis nisi exercitation culpa dolore sint sint. Aute cillum tempor labore amet officia occaecat incididunt reprehenderit laboris laborum ad reprehenderit ipsum. Laboris reprehenderit incididunt quis magna. Mollit ex esse reprehenderit exercitation proident excepteur. Pariatur non do veniam commodo nulla amet cupidatat.\r\n",
+          "registered": "2017-07-11T09:42:07 +05:00",
+          "latitude": -47.882637,
+          "longitude": -119.479734,
+          "tags": [
+            "eiusmod",
+            "ut",
+            "nisi",
+            "ex",
+            "excepteur",
+            "minim",
+            "veniam"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Shepard Parker"
+            },
+            {
+              "id": 1,
+              "name": "Hull Martinez"
+            },
+            {
+              "id": 2,
+              "name": "Thelma Estes"
+            }
+          ],
+          "greeting": "Hello, Susana Roberson! You have 1 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "643f0827e483aea472af90f2",
+          "index": 3,
+          "guid": "37c96237-f95d-443e-b70f-14cb1747a0e7",
+          "isActive": true,
+          "balance": "$3,903.65",
+          "picture": "http://placehold.it/32x32",
+          "age": 25,
+          "eyeColor": "green",
+          "name": "Annmarie Suarez",
+          "gender": "female",
+          "company": "PHARMEX",
+          "email": "annmariesuarez@pharmex.com",
+          "phone": "+1 (898) 557-3572",
+          "address": "673 Seaview Avenue, Spelter, Arizona, 8871",
+          "about": "Fugiat consectetur voluptate pariatur officia laboris amet laboris laboris labore reprehenderit pariatur elit occaecat pariatur. Consectetur ad ipsum ex ad veniam. Aliqua ea pariatur qui ullamco adipisicing aliqua mollit. Id duis occaecat officia occaecat deserunt aliquip elit excepteur eiusmod sit cupidatat anim culpa.\r\n",
+          "registered": "2021-08-17T04:47:09 +05:00",
+          "latitude": -13.902672,
+          "longitude": 164.267505,
+          "tags": [
+            "do",
+            "dolore",
+            "cupidatat",
+            "adipisicing",
+            "dolor",
+            "ex",
+            "proident"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "May Livingston"
+            },
+            {
+              "id": 1,
+              "name": "Shauna Donovan"
+            },
+            {
+              "id": 2,
+              "name": "Gill Wong"
+            }
+          ],
+          "greeting": "Hello, Annmarie Suarez! You have 8 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "643f0827c27ad7cf82c906a4",
+          "index": 4,
+          "guid": "11cbf218-a61a-48be-a4a7-3fc92686abec",
+          "isActive": false,
+          "balance": "$2,647.68",
+          "picture": "http://placehold.it/32x32",
+          "age": 27,
+          "eyeColor": "green",
+          "name": "Carver Smith",
+          "gender": "male",
+          "company": "NETUR",
+          "email": "carversmith@netur.com",
+          "phone": "+1 (921) 509-3271",
+          "address": "917 Cox Place, Dale, Hawaii, 7316",
+          "about": "Mollit quis commodo veniam deserunt laboris aute magna culpa elit ea id. Nostrud id ipsum officia ea id culpa mollit aliqua. Officia duis aliquip exercitation irure et anim consequat nulla. Quis eu velit exercitation nostrud aute.\r\n",
+          "registered": "2019-05-23T01:31:38 +05:00",
+          "latitude": 27.22406,
+          "longitude": -19.602432,
+          "tags": [
+            "veniam",
+            "esse",
+            "occaecat",
+            "est",
+            "exercitation",
+            "ex",
+            "qui"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Anita Murphy"
+            },
+            {
+              "id": 1,
+              "name": "Butler Maxwell"
+            },
+            {
+              "id": 2,
+              "name": "Shaw Marks"
+            }
+          ],
+          "greeting": "Hello, Carver Smith! You have 9 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "643f08272fbda623815d75c2",
+          "index": 5,
+          "guid": "66453ff6-6e27-496c-8cc9-535da46c8164",
+          "isActive": true,
+          "balance": "$1,018.03",
+          "picture": "http://placehold.it/32x32",
+          "age": 27,
+          "eyeColor": "green",
+          "name": "Mcgee Flynn",
+          "gender": "male",
+          "company": "UBERLUX",
+          "email": "mcgeeflynn@uberlux.com",
+          "phone": "+1 (853) 453-2541",
+          "address": "362 Jay Street, Greenbush, Iowa, 759",
+          "about": "Consequat sunt elit labore anim minim deserunt eu voluptate. Adipisicing quis ea voluptate consequat. Occaecat non adipisicing enim ea non commodo aute elit. Commodo laborum laborum est irure officia non. Nulla sunt veniam reprehenderit laborum commodo laboris do sunt cillum excepteur dolore irure nulla pariatur. Nulla fugiat officia cupidatat aliqua sint.\r\n",
+          "registered": "2017-09-25T01:26:39 +05:00",
+          "latitude": -1.9445,
+          "longitude": 48.778139,
+          "tags": [
+            "et",
+            "dolore",
+            "aute",
+            "culpa",
+            "tempor",
+            "sint",
+            "dolor"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Huffman Peck"
+            },
+            {
+              "id": 1,
+              "name": "Gross Wilcox"
+            },
+            {
+              "id": 2,
+              "name": "Hallie Carey"
+            }
+          ],
+          "greeting": "Hello, Mcgee Flynn! You have 6 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "643f08271440fea642af26b3",
+          "index": 6,
+          "guid": "7e88d89b-aeec-4af5-89de-3ed1eb5c20f9",
+          "isActive": true,
+          "balance": "$3,132.61",
+          "picture": "http://placehold.it/32x32",
+          "age": 39,
+          "eyeColor": "brown",
+          "name": "Florence Baxter",
+          "gender": "female",
+          "company": "HINWAY",
+          "email": "florencebaxter@hinway.com",
+          "phone": "+1 (812) 584-3388",
+          "address": "996 Joval Court, Bourg, Louisiana, 3199",
+          "about": "Irure magna qui qui aliqua quis sunt. Deserunt quis nulla in proident non. Tempor fugiat cupidatat proident duis nulla amet ut elit amet culpa consequat quis culpa occaecat. Cillum consequat culpa nostrud consequat excepteur est excepteur reprehenderit magna aliqua. Culpa minim irure consectetur incididunt dolor ut velit anim incididunt commodo ex officia id.\r\n",
+          "registered": "2021-12-18T03:49:20 +05:00",
+          "latitude": 18.806796,
+          "longitude": -21.914927,
+          "tags": [
+            "aute",
+            "elit",
+            "consectetur",
+            "excepteur",
+            "aliqua",
+            "aliquip",
+            "laboris"
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Barnett Farmer"
+            },
+            {
+              "id": 1,
+              "name": "Morrow Avery"
+            },
+            {
+              "id": 2,
+              "name": "Rosella Everett"
+            }
+          ],
+          "greeting": "Hello, Florence Baxter! You have 2 unread messages.",
+          "favoriteFruit": "apple"
+        }
+      
+]
